e5f2b7440b16b2b451e003a391f863cef7d02b4f
Nazar <nazaranddobriy@gmail.com>
2024-01-29 23:13:29 +0200

    fix: ignore:

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as ws from "ws";
 
-const PORT = 8080;
+const PORT = process.env.PORT ? Number(process.env.PORT) : 8080;
 
 const server = new ws.WebSocketServer({ port: PORT }, () => {
   console.log(`Server started on port ${PORT}`);
