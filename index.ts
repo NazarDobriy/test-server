9fe4557f6e5a509b88e138262d9c5f7715f88ff5
Nazar <nazaranddobriy@gmail.com>
2024-01-30 14:30:05 +0200

    feat: return change

--- index.ts
+++ index.ts
@@ -1,59 +1,4 @@
-const express = require('express');
-const http = require('http');
 const ws = require('ws');
-const fs = require('fs');
-
-const app = express();
-
-const PORT = process.env.PORT || 8080;
-
-const webSocketServer = new ws.WebSocketServer({ port: PORT }, () => { 
-  console.log(`Server started on port ${PORT}`); 
-}); 
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!\n');
-});
-
-// app.post('/process-array', (req, res) => {
-//   const dataArray = req.body;
-//   const filePath = 'output.txt';
-
-//   const fileContents = dataArray.join('\n');
-//   fs.writeFile(filePath, fileContents, (err) => {
-//     if (err) {
-//       console.error('Error writing to file:', err);
-//       res.status(500).send('Error writing to file');
-//     } else {
-//       console.log('File saved:', filePath);
-//       res.download(filePath, 'output.txt', (err) => {
-//         if (err) {
-//           console.error('Error sending file:', err);
-//           res.status(500).send('Error sending file');
-//         } else {
-//           console.log('File sent to client');
-//           fs.unlink(filePath, (err) => {
-//             if (err) {
-//               console.error('Error deleting file:', err);
-//             } else {
-//               console.log('File deleted:', filePath);
-//             }
-//           });
-//         }
-//       });
-//     }
-//   });
-// });
-
-webSocketServer.on('connection', socket => {
-  socket.on('message', message => {
-    socket.send(fizzBuzz(parseInt(message)));
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
 
 function fizzBuzz(n) {
   if (n % 15 === 0) {
@@ -66,3 +11,15 @@ function fizzBuzz(n) {
     return `${n}`;
   }
 }
+
+const PORT = process.env.PORT || 8080;
+
+const server = new ws.WebSocketServer({ port: PORT }, () => { 
+  console.log(`Server started on port ${PORT}`); 
+});
+
+server.on('connection', socket => {
+  socket.on('message', message => {
+    socket.send(fizzBuzz(parseInt(message)));
+  });
+});
