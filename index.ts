2b7f2bcf89534c94e2d5e0499fc6924d63169769
Nazar <nazaranddobriy@gmail.com>
2024-01-30 12:05:05 +0200

    feat: add fizzBuzz func

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import * as ws from "ws";
 
+function fizzBuzz(n: number): string {
+  if (n % 15 === 0) {
+    return 'FizzBuzz';
+  } else if (n % 3 === 0) {
+    return 'Fizz';
+  } else if (n % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return `${n}`;
+  }
+}
+
 const PORT = process.env.PORT || 8080;
 
 const server = new ws.WebSocketServer({ port: PORT }, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-server.on("connection", () => {
-  console.log('Connected!!!');
+server.on("connection", (message: string) => {
+  console.log(message);
 });
