22467f1361e056c14ad248aa803ae715a0232f1a
Nazar <nazaranddobriy@gmail.com>
2024-01-30 14:18:18 +0200

    fix: coment

--- index.ts
+++ index.ts
@@ -16,35 +16,35 @@ app.get('/', (req, res) => {
   res.send('Hello, world!\n');
 });
 
-app.post('/process-array', (req, res) => {
-  const dataArray = req.body;
-  const filePath = 'output.txt';
+// app.post('/process-array', (req, res) => {
+//   const dataArray = req.body;
+//   const filePath = 'output.txt';
 
-  const fileContents = dataArray.join('\n');
-  fs.writeFile(filePath, fileContents, (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      res.status(500).send('Error writing to file');
-    } else {
-      console.log('File saved:', filePath);
-      res.download(filePath, 'output.txt', (err) => {
-        if (err) {
-          console.error('Error sending file:', err);
-          res.status(500).send('Error sending file');
-        } else {
-          console.log('File sent to client');
-          fs.unlink(filePath, (err) => {
-            if (err) {
-              console.error('Error deleting file:', err);
-            } else {
-              console.log('File deleted:', filePath);
-            }
-          });
-        }
-      });
-    }
-  });
-});
+//   const fileContents = dataArray.join('\n');
+//   fs.writeFile(filePath, fileContents, (err) => {
+//     if (err) {
+//       console.error('Error writing to file:', err);
+//       res.status(500).send('Error writing to file');
+//     } else {
+//       console.log('File saved:', filePath);
+//       res.download(filePath, 'output.txt', (err) => {
+//         if (err) {
+//           console.error('Error sending file:', err);
+//           res.status(500).send('Error sending file');
+//         } else {
+//           console.log('File sent to client');
+//           fs.unlink(filePath, (err) => {
+//             if (err) {
+//               console.error('Error deleting file:', err);
+//             } else {
+//               console.log('File deleted:', filePath);
+//             }
+//           });
+//         }
+//       });
+//     }
+//   });
+// });
 
 webSocketServer.on('connection', socket => {
   socket.on('message', message => {
