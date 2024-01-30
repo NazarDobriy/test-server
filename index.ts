963fd400343e93f247594df86f307423f1221b56
Nazar <nazaranddobriy@gmail.com>
2024-01-30 13:53:44 +0200

    feat: add handle file

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const ws = require('ws');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +14,36 @@ const webSocketServer = new ws.WebSocketServer({ port: PORT }, () => {
 
 app.get('/', (req, res) => {
   res.send('Hello, world!\n');
+});
+
+app.post('/process-array', (req, res) => {
+  const dataArray = req.body;
+  const filePath = 'output.txt';
+
+  const fileContents = dataArray.join('\n');
+  fs.writeFile(filePath, fileContents, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      res.status(500).send('Error writing to file');
+    } else {
+      console.log('File saved:', filePath);
+      res.download(filePath, 'output.txt', (err) => {
+        if (err) {
+          console.error('Error sending file:', err);
+          res.status(500).send('Error sending file');
+        } else {
+          console.log('File sent to client');
+          fs.unlink(filePath, (err) => {
+            if (err) {
+              console.error('Error deleting file:', err);
+            } else {
+              console.log('File deleted:', filePath);
+            }
+          });
+        }
+      });
+    }
+  });
 });
 
 webSocketServer.on('connection', socket => {
